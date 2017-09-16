e3fac92cd56d38d891e6c491e9203167b13a9f91
Ying-Ta Lin <ytl287@nyu.edu>
2017-09-16 20:17:48 +0800

    got rid of saving the source_page.html

--- lizzy.py
+++ lizzy.py
@@ -6,7 +6,8 @@ from scrapy.selector import Selector
 
 class LizzySpider():
     def __init__(self):
-        self.filename = 'source_page.html'
+        # self.filename = 'source_page.html'
+        pass
 
     def log(self, entry):
         filename = 'data.json'
@@ -17,16 +18,6 @@ class LizzySpider():
         json.dump(entry, file)
         file.write(os.linesep)
         file.close()
-
-    def get_source(self):
-        url = "https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw=antique&rt=nc&LH_Auction=1"
-        source_code = requests.get(url)
-        plaintext = source_code.text
-
-        with open(self.filename, 'wb') as f:
-            f.write(plaintext.encode("UTF-8"))
-            f.close()
-        print 'Saved file: ' + self.filename
 
     def select_info(self, text):
         price = Selector(text=text).xpath('//li[@class=$val]/span/text()', val='lvprice prc').extract()
@@ -46,11 +37,23 @@ class LizzySpider():
         # log entries to a log file
         self.log(scraped_info)
 
+    def get_source(self):
+        url = "https://www.ebay.com/sch/i.html?_from=R40&_sacat=0&_nkw=antique&rt=nc&LH_Auction=1"
+        source_code = requests.get(url)
+        plaintext = source_code.text
+
+        # with open(self.filename, 'wb') as f:
+        #     f.write(plaintext.encode("UTF-8"))
+        #     f.close()
+        # print 'Saved file: ' + self.filename
+
+        self.select_info(plaintext)
+
     def scrap(self):
         self.get_source()
-        file = open(self.filename, 'r')
-        text = file.read()
-        self.select_info(text)
+        # file = open(self.filename, 'r')
+        # text = file.read()
+        # self.select_info(text)
 
 
 test = LizzySpider()
